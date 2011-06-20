b966715594afd4b3c0af10dd8e4ed98025f597c8
nana ansah baidoo <stylish17@ymail.com>
2011-06-20 13:39:17 +0000

    Update to First-Team-Project

---
+++ Team.py
@@ -0,0 +1,25 @@
+class Team:
+
+    def __init__(self):
+        """ Asks for the user input for a noun and stores the noun in
+        the instance variable self.word.  Remember a noun is a person,
+        place, or thing. """
+        # TODO by person 3
+        pass
+
+    def reverse_input(self):
+        """ Changes self.word to its reverse.  For example if
+        self.word is 'apples', then it becomes 'selppa'."""
+        # TODO by person 1
+        pass
+    
+    def print_in_sentence(self):
+        """ Insert self.word in the sentence 'Today I dreamt of
+        <self.word> while walking on the beach.' replacing <self.word>
+        for the noun that was chosen during class construction. """
+	  # TODO by person 2
+        pass
+
+t = Team()
+t.reverse_input()
+t.print_in_sentence()
