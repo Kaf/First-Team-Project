e7018cf909b333f6c8c41d7f4d018b1cfe63b2af
Kaf <a.kafu@yahoo.com>
2011-06-20 08:13:49 -0700

    Edited Team.py via GitHub

--- Team.py
+++ Team.py
@@ -5,13 +5,15 @@ class Team:
         the instance variable self.word.  Remember a noun is a person,
         place, or thing. """
         # TODO by person 3
-        pass
+        
 
     def reverse_input(self):
         """ Changes self.word to its reverse.  For example if
         self.word is 'apples', then it becomes 'selppa'."""
         # TODO by person 1
-        pass
+        reverse =self.word[::-1]
+        print reverse
+
     
     def print_in_sentence(self):
         """ Insert self.word in the sentence 'Today I dreamt of
